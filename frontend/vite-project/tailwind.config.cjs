1f21651ccc7d035598ab0577310ac1b26c332f92
unknown <anigandini@gmail.com>
2026-03-25 12:56:53 -0300

    update config files

---
+++ frontend/vite-project/tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx, cjs}'
+  ],
+  theme: {
+      colors: {
+        primary: '#0F172A', //titles
+        secondary: '#64748B', //borders and text
+        accent: '#5048E5', //buttons and highlights
+        light: '#f6f6f8', //background
+        medium: '#E2E8F0', //shadows and dividers
+      },
+  },
+  plugins: [],
+}
